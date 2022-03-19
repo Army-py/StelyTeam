a14a7b1123679215fb710b1e06319c09c22b2719
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-03-19 23:38:17 +0100

    Don't perform clear operations on the PlayerList if it's empty

--- src/main/java/fr/army/stelyteam/team/PlayerList.java
+++ src/main/java/fr/army/stelyteam/team/PlayerList.java
@@ -41,6 +41,9 @@ public class PlayerList implements IPlayerList {
     }
 
     public void clear() {
+        if (uuids.isEmpty()) {
+            return;
+        }
         for (UUID id : uuids) {
             playerTeamTracker.changeTeam(id, null);
         }
