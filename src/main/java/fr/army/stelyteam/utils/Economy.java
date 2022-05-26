55c73856e86646647805dbcb0b48b950fb4c91e2
Army-py <61599555+Army-py@users.noreply.github.com>
2022-05-26 18:08:59 +0200

    Remove MySQLDataBridge

--- src/main/java/fr/army/stelyteam/utils/Economy.java
+++ src/main/java/fr/army/stelyteam/utils/Economy.java
@@ -3,15 +3,18 @@ package fr.army.stelyteam.utils;
 import org.bukkit.entity.Player;
 
 import fr.army.stelyteam.StelyTeamPlugin;
-import net.craftersland.data.bridge.PD;
 
 
 public class Economy {
     public boolean checkMoneyPlayer(Player player, int money) {
-        return PD.api.getDatabaseMoney(player.getName()) >= ((double) money);
+        return StelyTeamPlugin.economy.getBalance(player) >= ((double) money);
     }
 
     public void removeMoneyPlayer(Player player, int money) {
         StelyTeamPlugin.economy.withdrawPlayer(player, money);
+    }
+
+    public void addMoneyPlayer(Player player, int money) {
+        StelyTeamPlugin.economy.depositPlayer(player, money);
     }
 }
