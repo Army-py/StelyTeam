13120eb486d61b8241aa6ebe1377f8684123e043
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-05-20 22:25:21 +0200

    Fix wrong call in ListenerLoader#registerListeners
    Fix wrong call in ListenerLoader

--- src/main/java/fr/army/stelyteam/listener/ListenerLoader.java
+++ src/main/java/fr/army/stelyteam/listener/ListenerLoader.java
@@ -8,7 +8,7 @@ public class ListenerLoader {
 
     public void registerListeners(StelyTeamPlugin plugin) {
         final PluginManager pluginManager = Bukkit.getPluginManager();
-        Bukkit.getPluginManager().registerEvents(new InventoryClickListener(plugin), plugin);
+        pluginManager.registerEvents(new InventoryClickListener(plugin), plugin);
         pluginManager.registerEvents(new InventoryCloseListener(plugin), plugin);
         pluginManager.registerEvents(new PlayerJoinListener(plugin), plugin);
         pluginManager.registerEvents(new PlayerQuitListener(plugin), plugin);
