c26733859e9c66242b56ce44beb633bd646f943b
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-05-20 22:41:44 +0200

    Add PlayerTeamChatEvent

---
+++ src/main/java/fr/army/stelyteam/api/event/PlayerTeamChatEvent.java
@@ -0,0 +1,67 @@
+package fr.army.stelyteam.api.event;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerTeamChatEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancel = false;
+    private final Player sender;
+    private String format;
+    private String message;
+
+    public PlayerTeamChatEvent(boolean async, @NotNull Player sender, @NotNull String format, @NotNull String message) {
+        super(async);
+        this.sender = sender;
+        this.format = format;
+        this.message = message;
+    }
+
+    public Player getSender() {
+        return sender;
+    }
+
+    @NotNull
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    @NotNull
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(@NotNull String message) {
+        this.message = message;
+    }
+
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+}
