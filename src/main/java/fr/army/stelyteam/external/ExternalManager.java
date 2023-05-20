c8b4d6fa1a856bb9384cfa741f4735e5f89f9ae5
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-05-20 23:48:56 +0200

    Remove BungeeChatConnect imports in ExternalManager

--- src/main/java/fr/army/stelyteam/external/ExternalManager.java
+++ src/main/java/fr/army/stelyteam/external/ExternalManager.java
@@ -1,13 +1,5 @@
 package fr.army.stelyteam.external;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
-
-import org.bukkit.plugin.Plugin;
-
-import fr.army.stelyteam.external.bungeechatconnect.ExternalBCCLoader;
-import fr.army.stelyteam.external.bungeechatconnect.handler.RecipientsHandler;
 import fr.army.stelyteam.external.stelyclaim.StelyClaimLoader;
 
 public class ExternalManager {
