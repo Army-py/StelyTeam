a5955ca7b914b1bfd8cae1d82c1561e644c6f118
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-05-29 16:02:27 +0200

    Update SubCmdEditPrefix to use TeamManager

--- src/main/java/fr/army/stelyteam/command/subcommand/manage/SubCmdEditPrefix.java
+++ src/main/java/fr/army/stelyteam/command/subcommand/manage/SubCmdEditPrefix.java
@@ -1,11 +1,10 @@
 package fr.army.stelyteam.command.subcommand.manage;
 
 import fr.army.stelyteam.StelyTeamPlugin;
-import fr.army.stelyteam.cache.StorageManager;
-import fr.army.stelyteam.cache.TeamCache;
 import fr.army.stelyteam.cache.TeamField;
 import fr.army.stelyteam.command.SubCommand;
 import fr.army.stelyteam.team.Team;
+import fr.army.stelyteam.team.TeamManager;
 import fr.army.stelyteam.utils.builder.ColorsBuilder;
 import fr.army.stelyteam.utils.manager.MessageManager;
 import org.bukkit.command.CommandSender;
@@ -17,15 +16,13 @@ import java.util.List;
 
 public class SubCmdEditPrefix extends SubCommand {
 
-    private final StorageManager storageManager;
-    private final TeamCache teamCache;
+    private final TeamManager teamManager;
     private final MessageManager messageManager;
     private final ColorsBuilder colorsBuilder;
 
     public SubCmdEditPrefix(@NotNull StelyTeamPlugin plugin) {
         super(plugin);
-        storageManager = plugin.getStorageManager();
-        teamCache = plugin.getTeamCache();
+        teamManager = plugin.getTeamManager();
         this.messageManager = plugin.getMessageManager();
         this.colorsBuilder = new ColorsBuilder(plugin);
     }
@@ -38,19 +35,14 @@ public class SubCmdEditPrefix extends SubCommand {
             player.sendMessage(messageManager.getMessage("commands.stelyteam_editprefix.usage"));
             return true;
         }
-        Team team = storageManager.retreiveTeam(args[1], TeamField.PREFIX);
+        final Team team = teamManager.getTeam(args[1], TeamField.PREFIX);
         if (team == null) {
             player.sendMessage(messageManager.getMessage("common.team_not_exist"));
             return true;
         }
-        final Team cachedTeam = teamCache.getTeam(team.getId());
-        if (cachedTeam != null) {
-            cachedTeam.getPrefix().retrieve(team.getId(), storageManager);
-            team = cachedTeam;
-        }
         final String newPrefix = args[2];
         team.getPrefix().set(newPrefix);
-        team.getPrefix().save(team.getId(), storageManager);
+        teamManager.saveTeam(team);
         player.sendMessage(messageManager.getReplaceMessage("commands.stelyteam_editprefix.output", colorsBuilder.replaceColor(newPrefix)));
         return true;
     }
