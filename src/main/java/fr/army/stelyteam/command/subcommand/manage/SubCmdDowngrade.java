22f072cbef219d0d8745176b121bbd0225107d5c
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-05-29 15:54:56 +0200

    Update SubCmdDowngrade to use TeamManager

--- src/main/java/fr/army/stelyteam/command/subcommand/manage/SubCmdDowngrade.java
+++ src/main/java/fr/army/stelyteam/command/subcommand/manage/SubCmdDowngrade.java
@@ -2,11 +2,10 @@ package fr.army.stelyteam.command.subcommand.manage;
 
 import fr.army.stelyteam.StelyTeamPlugin;
 import fr.army.stelyteam.cache.Property;
-import fr.army.stelyteam.cache.StorageManager;
-import fr.army.stelyteam.cache.TeamCache;
 import fr.army.stelyteam.cache.TeamField;
 import fr.army.stelyteam.command.SubCommand;
 import fr.army.stelyteam.team.Team;
+import fr.army.stelyteam.team.TeamManager;
 import fr.army.stelyteam.utils.manager.MessageManager;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -17,15 +16,13 @@ import java.util.List;
 
 public class SubCmdDowngrade extends SubCommand {
 
-    private final StorageManager storageManager;
-    private final TeamCache teamCache;
+    private final TeamManager teamManager;
     private final MessageManager messageManager;
 
 
     public SubCmdDowngrade(@NotNull StelyTeamPlugin plugin) {
         super(plugin);
-        storageManager = plugin.getStorageManager();
-        teamCache = plugin.getTeamCache();
+        teamManager = plugin.getTeamManager();
         this.messageManager = plugin.getMessageManager();
     }
 
@@ -36,18 +33,13 @@ public class SubCmdDowngrade extends SubCommand {
             player.sendMessage(messageManager.getMessage("commands.stelyteam_downgrade.usage"));
             return true;
         }
-        args[0] = "";
 
+        args[0] = "";
         final String teamName = String.join("", args);
-        Team team = storageManager.retreiveTeam(teamName, TeamField.UPGRADES_MEMBERS);
+        final Team team = teamManager.getTeam(teamName, TeamField.UPGRADES_MEMBERS);
         if (team == null) {
             player.sendMessage(messageManager.getMessage("common.team_not_exist"));
             return true;
-        }
-        final Team cachedTeam = teamCache.getTeam(team.getId());
-        if (cachedTeam != null) {
-            cachedTeam.getMembers().retrieve(team.getId(), storageManager);
-            team = cachedTeam;
         }
         final Property<Integer> membersLevel = team.getUpgrades().getMembers();
         final Integer rawMemberLevel = membersLevel.get();
@@ -56,7 +48,7 @@ public class SubCmdDowngrade extends SubCommand {
             return true;
         }
         membersLevel.set(rawMemberLevel - 1);
-        membersLevel.save(team.getId(), storageManager);
+        teamManager.saveTeam(team);
         player.sendMessage(messageManager.getReplaceMessage("commands.stelyteam_downgrade.output", teamName));
         return true;
     }
