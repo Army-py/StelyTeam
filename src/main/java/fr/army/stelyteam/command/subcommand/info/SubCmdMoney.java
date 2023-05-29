970f12d78b3892ddc0a66ceaf6abaaaa99e37cae
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-05-29 13:44:06 +0200

    Update SubCmdMoney to use TeamManager

--- src/main/java/fr/army/stelyteam/command/subcommand/info/SubCmdMoney.java
+++ src/main/java/fr/army/stelyteam/command/subcommand/info/SubCmdMoney.java
@@ -1,10 +1,10 @@
 package fr.army.stelyteam.command.subcommand.info;
 
 import fr.army.stelyteam.StelyTeamPlugin;
-import fr.army.stelyteam.cache.StorageManager;
 import fr.army.stelyteam.cache.TeamField;
 import fr.army.stelyteam.command.SubCommand;
 import fr.army.stelyteam.team.Team;
+import fr.army.stelyteam.team.TeamManager;
 import fr.army.stelyteam.utils.manager.MessageManager;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,27 +16,26 @@ import java.util.Locale;
 
 public class SubCmdMoney extends SubCommand {
 
-    private final StorageManager storageManager;
+    private final TeamManager teamManager;
     //private DatabaseManager sqlManager;
     private final MessageManager messageManager;
 
     public SubCmdMoney(@NotNull StelyTeamPlugin plugin) {
         super(plugin);
-        this.storageManager = plugin.getStorageManager();
+        this.teamManager = plugin.getTeamManager();
         //this.sqlManager = plugin.getDatabaseManager();
         this.messageManager = plugin.getMessageManager();
     }
 
     @Override
     public boolean execute(@NotNull Player player, @NotNull String @NotNull [] args) {
-        args[0] = "";
-
         if (args.length == 1) {
             player.sendMessage(messageManager.getMessage("commands.stelyteam_money.usage"));
             return true;
         }
+        args[0] = "";
         final String teamName = String.join("", args);
-        final Team team = storageManager.retreiveTeam(teamName, TeamField.BANK_BALANCE);
+        final Team team = teamManager.getTeam(teamName, TeamField.BANK_BALANCE);
         if (team == null) {
             player.sendMessage(messageManager.getMessage("common.team_not_exist"));
             return true;
@@ -47,7 +46,7 @@ public class SubCmdMoney extends SubCommand {
         return true;
     }
 
-    private String DoubleToString(double value){
+    private String DoubleToString(double value) {
         return NumberFormat.getNumberInstance(Locale.US).format(value);
     }
 
