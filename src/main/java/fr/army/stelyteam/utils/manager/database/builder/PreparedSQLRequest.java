6d379355839c46806fd8753414953ea84918d144
Army-py <61599555+Army-py@users.noreply.github.com>
2023-06-02 18:27:54 +0200

    Edit request sql builder

--- src/main/java/fr/army/stelyteam/utils/manager/database/builder/PreparedSQLRequest.java
+++ src/main/java/fr/army/stelyteam/utils/manager/database/builder/PreparedSQLRequest.java
@@ -5,14 +5,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class PreparedSQLRequest<T> {
+public class PreparedSQLRequest {
     
     private final Connection conn;
     private final FundamentalOperator fundamentalOperator;
 
     private PreparedStatement preparedStatement;
 
-    private T result;
+    private ResultSet result;
 
     public PreparedSQLRequest(Connection conn, FundamentalOperator fundamentalOperator){
         this.conn = conn;
@@ -20,13 +20,13 @@ public class PreparedSQLRequest<T> {
     }
 
 
-    public PreparedSQLRequest<T> build() throws SQLException{
+    public PreparedSQLRequest build() throws SQLException{
         preparedStatement = conn.prepareStatement(fundamentalOperator.build());
         return this;
     }
 
-    public PreparedSQLRequest<T> execute() throws SQLException{
-        ResultSet result = preparedStatement.executeQuery();
+    public PreparedSQLRequest execute() throws SQLException{
+        result = preparedStatement.executeQuery();
         return this;
     }
 }
