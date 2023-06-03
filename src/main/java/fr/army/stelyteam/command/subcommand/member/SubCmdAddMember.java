791d98f8102a06f7c6d10bc84eba5ede3fd52f5b
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-03 18:54:53 +0200

    Implement SubCmdAddMember with the new SetProperty

--- src/main/java/fr/army/stelyteam/command/subcommand/member/SubCmdAddMember.java
+++ src/main/java/fr/army/stelyteam/command/subcommand/member/SubCmdAddMember.java
@@ -2,28 +2,33 @@ package fr.army.stelyteam.command.subcommand.member;
 
 import fr.army.stelyteam.StelyTeamPlugin;
 import fr.army.stelyteam.cache.SaveField;
+import fr.army.stelyteam.cache.TeamCache;
 import fr.army.stelyteam.command.SubCommand;
+import fr.army.stelyteam.team.Member;
 import fr.army.stelyteam.team.Team;
 import fr.army.stelyteam.team.TeamManager;
+import fr.army.stelyteam.utils.OfflinePlayerRetriever;
 import fr.army.stelyteam.utils.manager.MessageManager;
-import fr.army.stelyteam.utils.manager.database.DatabaseManager;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class SubCmdAddMember extends SubCommand {
 
     private final TeamManager teamManager;
-    private final DatabaseManager sqlManager;
+    private final TeamCache teamCache;
     private final MessageManager messageManager;
 
     public SubCmdAddMember(@NotNull StelyTeamPlugin plugin) {
         super(plugin);
         teamManager = plugin.getTeamManager();
-        this.sqlManager = plugin.getDatabaseManager();
+        teamCache = plugin.getTeamCache();
         this.messageManager = plugin.getMessageManager();
     }
 
@@ -40,12 +45,16 @@ public class SubCmdAddMember extends SubCommand {
             player.sendMessage(messageManager.getMessage("common.team_not_exist"));
             return true;
         }
-        // TODO Member work
-        if (sqlManager.isMember(args[2])) {
+        final Optional<Team> currentTeam = teamCache.getPlayerTeam(player.getUniqueId());
+        if (currentTeam.isPresent()) {
             player.sendMessage(messageManager.getMessage("common.player_already_in_team"));
             return true;
         }
-        team.getMembers().add(args[2]);
+        final OfflinePlayer target = OfflinePlayerRetriever.getOfflinePlayer(args[2]);
+        final Member member = new Member(target.getUniqueId());
+        member.getJoiningDate().set(Calendar.getInstance().getTime());
+        // TODO Maybe set the rank
+        team.getMembers().add(member);
         teamManager.saveTeam(team);
         player.sendMessage(messageManager.getReplaceMessage("commands.stelyteam_addmember.output", args[2]));
         return true;
