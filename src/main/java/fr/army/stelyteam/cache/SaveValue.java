574cb256d840dce91c638c51c76493ef1f1da1fa
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-03 17:07:53 +0200

    Add SaveProperty and SaveSet

--- src/main/java/fr/army/stelyteam/cache/SaveValue.java
+++ src/main/java/fr/army/stelyteam/cache/SaveValue.java
@@ -2,5 +2,23 @@ package fr.army.stelyteam.cache;
 
 import org.jetbrains.annotations.NotNull;
 
-public record SaveValue<T>(@NotNull SaveField field, T value) {
+public class SaveValue<T> extends SaveProperty<T> {
+
+    private final T value;
+
+    public SaveValue(@NotNull SaveField field, @NotNull PropertiesHolder holder, @NotNull T value) {
+        super(field, holder);
+        this.value = value;
+    }
+
+    @Override
+    public @NotNull SaveValue<T> asValue() {
+        return this;
+    }
+
+    @NotNull
+    public T getValue() {
+        return value;
+    }
+
 }
