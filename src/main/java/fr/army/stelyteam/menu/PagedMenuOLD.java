8ecc843d4dd1311fa622f9225f008b2bdc6d1000
Army-py <61599555+Army-py@users.noreply.github.com>
2023-09-10 14:25:53 +0200

    ~ OLD class

--- src/main/java/fr/army/stelyteam/menu/PagedMenuOLD.java
+++ src/main/java/fr/army/stelyteam/menu/PagedMenuOLD.java
@@ -2,13 +2,13 @@ package fr.army.stelyteam.menu;
 
 import org.bukkit.entity.Player;
 
-public abstract class PagedMenu extends TeamMenuOLD {
+public abstract class PagedMenuOLD extends TeamMenuOLD {
 
-    public PagedMenu(Player viewer, int menuSlots, TeamMenuOLD previousMenu) {
+    public PagedMenuOLD(Player viewer, int menuSlots, TeamMenuOLD previousMenu) {
         super(viewer, menuSlots, previousMenu);
     }
     
-    public PagedMenu(Player viewer, String menuName, int menuSlots, TeamMenuOLD previousMenu) {
+    public PagedMenuOLD(Player viewer, String menuName, int menuSlots, TeamMenuOLD previousMenu) {
         super(viewer, menuName, menuSlots, previousMenu);
     }
 
