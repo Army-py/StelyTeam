f480a4667b0fb0688f612dc8cb96d66272b433a4
Army-py <61599555+Army-py@users.noreply.github.com>
2023-09-18 20:09:06 +0000

    ~ Add load of some config

--- src/main/java/fr/army/stelyteam/config/Config.java
+++ src/main/java/fr/army/stelyteam/config/Config.java
@@ -1,5 +1,27 @@
 package fr.army.stelyteam.config;
 
-public enum Config {
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class Config {
+
+    private final YamlConfiguration config;
     
+    public static String language;
+    public static String[] serverNames;
+    public static boolean enableTeamChat;
+    public static String teamChatFormat;
+
+
+    public Config(YamlConfiguration config) {
+        this.config = config;
+    }
+
+
+    public void load(){
+        language = this.config.getString("language");
+        serverNames = this.config.getStringList("server-names").toArray(new String[0]);
+        // TODO: voir pour changer new String[0] par String[]::new
+        enableTeamChat = this.config.getBoolean("team-chat.enable");
+        teamChatFormat = this.config.getString("team-chat.format");
+    }
 }
