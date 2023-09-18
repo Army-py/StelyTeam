251ce747d3e44da0d77b612ea2041bae57ebfd8c
Army-py <61599555+Army-py@users.noreply.github.com>
2023-09-18 17:59:54 +0000

    + Add enum Placeholders

---
+++ src/main/java/fr/army/stelyteam/config/message/Placeholders.java
@@ -0,0 +1,16 @@
+package fr.army.stelyteam.config.message;
+
+public enum Placeholders {
+
+    TEAM_NAME,
+    TEAM_PREFIX,
+    TEAM_DESCRIPTION,
+    PLAYER_NAME,
+    PLAYER_DISPLAY_NAME,
+    TEAM_OWNER,
+    TEAM_UNLOCKED_BANK,
+    TEAM_MEMBER_COUNT,
+    TEAM_MEMBERS,
+    TEAM_CREATION_DATE,
+    ;
+}
