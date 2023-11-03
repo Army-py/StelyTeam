809c4fcfc10ec9d606301782eaca349910471dfb
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-03 16:09:14 +0100

    ~ Remove useless comment

--- src/main/java/fr/army/stelyteam/menu/impl/CreateTeamMenu.java
+++ src/main/java/fr/army/stelyteam/menu/impl/CreateTeamMenu.java
@@ -33,6 +33,10 @@ public class CreateTeamMenu extends TeamMenu<MenuView> {
         return new MenuView(player, this);
     }
 
+    @Override
+    public void onClick(InventoryClickEvent clickEvent) {
+    }
+
     public static CreateTeamMenu createInstance(String configName) {
         final MenuBuilderResult<MenuView> builderResult = MenuBuilder.getInstance().loadMenu(configName + ".yml");
         final MenuTemplate<MenuView> menuTemplate = builderResult.getMenuTemplate();
@@ -61,98 +65,8 @@ public class CreateTeamMenu extends TeamMenu<MenuView> {
             }
 
             menuTemplate.mapButtons(menuTemplate.getSlots(chr), button);
-
-            // if (itemSection.getString("button-type").equals(Buttons.BUTTON_CREATE_TEAM.toString())) {
-            //     ConfigurationSection sellSection = itemSection.getConfigurationSection("sell");
-
-            //     final Material material = Material.getMaterial(sellSection.getString("material"));
-            //     final String name = sellSection.getString("name");
-            //     final String skullTexture = sellSection.getString("skull-texture") == null
-            //             || sellSection.getString("skull-texture").isEmpty() ? null
-            //                     : sellSection.getString("skull-texture");
-            //     final int price = sellSection.getInt("price");
-            //     final int quantity = sellSection.getInt("quantity");
-
-            //     for (int slot : menu.getSlots(section)) {
-            //         final SellingButton button = SellingButton.mapButton(menu.getButton(slot));
-            //         button.setSellingMaterial(material);
-            //         button.setSellingItemName(name);
-            //         button.setSellingSkullTexture(skullTexture);
-            //         button.setSellingPrice(price);
-            //         button.setSellingQuantity(quantity);
-            //         button.setNPCName(npcName);
-            //         menu.mapButton(slot, button);
-            //     }
-            // }else if (itemSection.getString("button-type").equals(ButtonType.BUYING.toString())) {
-            //     ConfigurationSection buyingSection = itemSection.getConfigurationSection("buy");
-
-            //     final Material material = Material.getMaterial(buyingSection.getString("material"));
-            //     final String name = buyingSection.getString("name");
-            //     final String skullTexture = buyingSection.getString("skull-texture") == null
-            //             || buyingSection.getString("skull-texture").isEmpty() ? null
-            //                     : buyingSection.getString("skull-texture");
-            //     final int price = buyingSection.getInt("price");
-            //     final int quantity = buyingSection.getInt("quantity");
-
-            //     for (int slot : menu.getSlots(section)) {
-            //         // final BuyingButton button = BuyingButton.mapButton(menu.getButton(slot))
-            //         //         .setBuyingMaterial(material)
-            //         //         .setBuyingItemName(name)
-            //         //         .setBuyingSkullTexture(skullTexture)
-            //         //         .setBuyingPrice(price)
-            //         //         .setBuyingQuantity(quantity)
-            //         //         .setNPCName(npcName);
-            //         final BuyingButton.Builder button = new BuyingButton.Builder();
-            //         menu.mapButton(slot, button);
-            //     }
-            // } else if (itemSection.getString("button-type").equals(ButtonType.BACK.toString())) {
-            //     for (int slot : menu.getSlots(section)) {
-            //         final BackButton button = BackButton.mapButton(menu.getButton(slot));
-            //         menu.setButton(slot, button);
-            //     }
-            // }
         }
 
         return new CreateTeamMenu(builderResult);
-    }
-
-
-    // public Inventory createInventory() {
-	// 	Inventory inventory = Bukkit.createInventory(this, this.menuSlots, this.menuName);
-		
-    //     emptyCases(inventory, this.menuSlots, 0);
-
-    //     for(String buttonName : config.getConfigurationSection("inventories.createTeam").getKeys(false)){
-    //         Integer slot = config.getInt("inventories.createTeam."+buttonName+".slot");
-    //         Material material = Material.getMaterial(config.getString("inventories.createTeam."+buttonName+".itemType"));
-    //         String displayName = config.getString("inventories.createTeam."+buttonName+".itemName");
-    //         List<String> lore = config.getStringList("inventories.createTeam."+buttonName+".lore");
-    //         String headTexture = config.getString("inventories.createTeam."+buttonName+".headTexture");
-    //         inventory.setItem(slot, ItemBuilder.getItem(material, buttonName, displayName, lore, headTexture, false));
-    //     }
-		
-	// 	return inventory;
-    // }
-
-
-    @Override
-    public void onClick(InventoryClickEvent clickEvent) {
-        // Player player = (Player) clickEvent.getWhoClicked();
-        // String playerName = player.getName();
-
-        // if (Buttons.CREATE_TEAM_BUTTON.isClickedButton(clickEvent)){
-        //     if (plugin.getEconomyManager().checkMoneyPlayer(player, config.getDouble("prices.createTeam"))){
-        //         new ConfirmMenu(viewer, this).openMenu();
-        //         plugin.getCacheManager().addTempAction(
-        //             new TemporaryAction(
-        //                 playerName,
-        //                 TemporaryActionNames.CREATE_TEAM,
-        //                 Team.initFromPlayerName(playerName)));
-        //     }else{
-        //         player.sendMessage(plugin.getMessageManager().getMessage("common.not_enough_money"));
-        //     }
-        // }else if (Buttons.TEAM_LIST_MENU_BUTTON.isClickedButton(clickEvent)){
-        //     new TeamListMenu(viewer, this).openMenu(0);
-        // }
     }
 }
