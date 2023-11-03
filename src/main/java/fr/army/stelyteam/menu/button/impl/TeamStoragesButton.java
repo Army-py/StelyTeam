208c5bb295fe593931ba306cbb4217c0e29680c8
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-03 15:57:08 +0100

    + Add TeamStoragesButton class

---
+++ src/main/java/fr/army/stelyteam/menu/button/impl/TeamStoragesButton.java
@@ -0,0 +1,17 @@
+package fr.army.stelyteam.menu.button.impl;
+
+import fr.army.stelyteam.menu.button.Button;
+import fr.army.stelyteam.menu.button.template.ButtonTemplate;
+import fr.army.stelyteam.menu.view.TeamMenuView;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class TeamStoragesButton extends Button<TeamMenuView> {
+    public TeamStoragesButton(ButtonTemplate buttonTemplate) {
+        super(buttonTemplate);
+    }
+
+    @Override
+    public void onClick(InventoryClickEvent clickEvent) {
+        // TODO: open team storages menu
+    }
+}
