7cd993250259e943ee2fe2e263ef8c6b7258cc3b
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-21 21:49:23 +0100

    ~ Fill and edit alliances menu constant

--- src/main/java/fr/army/stelyteam/menu/Menus.java
+++ src/main/java/fr/army/stelyteam/menu/Menus.java
@@ -1,8 +1,11 @@
 package fr.army.stelyteam.menu;
 
+import fr.army.stelyteam.menu.impl.AlliancesMenu;
 import fr.army.stelyteam.menu.impl.ConfirmCreateTeamMenu;
 import fr.army.stelyteam.menu.impl.CreateTeamMenu;
 import fr.army.stelyteam.menu.view.impl.MenuView;
+import fr.army.stelyteam.menu.view.impl.PagedMenuView;
+import fr.army.stelyteam.team.Alliance;
 
 public class Menus {
     
@@ -12,7 +15,7 @@ public class Menus {
     public static TeamMenu MENU_TEAM_MANAGE;
     public static TeamMenu MENU_HOME; // previously MEMBER_MENU
     public static TeamMenu MENU_TEAM_MEMBERS;
-    public static TeamMenu MENU_TEAM_ALLIANCES;
+    public static TeamMenu<PagedMenuView<Alliance>> MENU_TEAM_ALLIANCES;
     public static TeamMenu MENU_MEMBERS_MANAGE;
     public static TeamMenu MENU_ALLIANCES_MANAGE;
     public static TeamMenu MENU_IMPROVE_MEMBER_LENGTH;
@@ -25,5 +28,6 @@ public class Menus {
     public void init() {
         MENU_CREATE_TEAM = CreateTeamMenu.createInstance("create_team");
         MENU_CONFIRM_CREATE_TEAM = ConfirmCreateTeamMenu.createInstance("confirm_create_team");
+        MENU_TEAM_ALLIANCES = AlliancesMenu.createInstance("team_alliances");
     }
 }
