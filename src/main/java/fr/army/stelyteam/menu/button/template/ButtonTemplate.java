9782faf45eb9429f97922dcbdbd290681ff7d56c
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-21 21:49:55 +0100

    ~ Annotations added

--- src/main/java/fr/army/stelyteam/menu/button/template/ButtonTemplate.java
+++ src/main/java/fr/army/stelyteam/menu/button/template/ButtonTemplate.java
@@ -10,22 +10,21 @@ public class ButtonTemplate {
     private final char character;
     private ButtonItem buttonItem;
 
-    public ButtonTemplate(@NotNull char character, @Nullable ButtonItem buttonItem) {
+    public ButtonTemplate(char character, @NotNull ButtonItem buttonItem) {
         this.character = character;
         this.buttonItem = buttonItem;
     }
 
-    @NotNull
     public char getCharacter() {
         return character;
     }
 
-    @Nullable
+    @NotNull
     public ButtonItem getButtonItem() {
         return buttonItem;
     }
 
-    public ButtonTemplate setButtonItem(ButtonItem buttonItem) {
+    public ButtonTemplate setButtonItem(@NotNull ButtonItem buttonItem) {
         this.buttonItem = buttonItem;
         return this;
     }
