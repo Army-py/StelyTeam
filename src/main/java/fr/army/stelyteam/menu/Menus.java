7e2a7268d9840bd39dc10a3eb8e03ca4de5a43df
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-23 18:53:21 +0100

    ~ Add constant to StoragesMenu

--- src/main/java/fr/army/stelyteam/menu/Menus.java
+++ src/main/java/fr/army/stelyteam/menu/Menus.java
@@ -1,13 +1,11 @@
 package fr.army.stelyteam.menu;
 
-import fr.army.stelyteam.menu.impl.AlliancesMenu;
-import fr.army.stelyteam.menu.impl.ConfirmCreateTeamMenu;
-import fr.army.stelyteam.menu.impl.CreateTeamMenu;
-import fr.army.stelyteam.menu.impl.MembersMenu;
+import fr.army.stelyteam.menu.impl.*;
 import fr.army.stelyteam.menu.view.impl.MenuView;
 import fr.army.stelyteam.menu.view.impl.PagedMenuView;
 import fr.army.stelyteam.team.Alliance;
 import fr.army.stelyteam.team.Member;
+import fr.army.stelyteam.team.Storage;
 
 public class Menus {
     
@@ -23,7 +21,7 @@ public class Menus {
     public static TeamMenu MENU_IMPROVE_MEMBER_LENGTH;
     public static TeamMenu MENU_IMPROVE_STORAGE_LENGTH;
     public static TeamMenu MENU_TEAM_PERMISSIONS;
-    public static TeamMenu MENU_STORAGE_DIRECTORY;
+    public static TeamMenu<PagedMenuView<Storage>> MENU_TEAM_STORAGES;
     public static TeamMenu MENU_STORAGE;
     
 
@@ -32,5 +30,6 @@ public class Menus {
         MENU_CONFIRM_CREATE_TEAM = ConfirmCreateTeamMenu.createInstance("confirm_create_team");
         MENU_TEAM_MEMBERS = MembersMenu.createInstance("team_members");
         MENU_TEAM_ALLIANCES = AlliancesMenu.createInstance("team_alliances");
+        MENU_TEAM_STORAGES = StoragesMenu.createInstance("team_storages");
     }
 }
