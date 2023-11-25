855f0b59949c1eee7f52729700d5ebc225c1df4d
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-25 15:40:33 +0100

    ~ Remove useless method due to the setup of EMFLoader

--- src/main/java/fr/army/stelyteam/controller/AbstractController.java
+++ src/main/java/fr/army/stelyteam/controller/AbstractController.java
@@ -1,41 +1,29 @@
 package fr.army.stelyteam.controller;
 
-import fr.army.stelyteam.StelyTeamPlugin;
 import fr.army.stelyteam.controller.callback.AsyncCallBackExceptionHandler;
 import fr.army.stelyteam.controller.callback.AsyncCallBackObject;
 import fr.army.stelyteam.controller.callback.AsyncCallBackObjectList;
 import fr.army.stelyteam.controller.exception.ControllerException;
-import fr.army.stelyteam.controller.exception.impl.EntityManagerNotInitializedException;
 import fr.army.stelyteam.controller.exception.impl.EntityNotFoundException;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
-import java.util.Properties;
 
 public abstract class AbstractController<T> {
 
     private final Class<T> entityClass;
-    private static EntityManagerFactory entityManagerFactory;
+    private final EntityManager entityManager;
 
-    private static final boolean remoteDatabase = false;
-    private static final String databaseHost = "localhost";
-    private static final int databasePort = 3306;
-    private static final String databaseSchema = "schema";
-    private static final String databaseUsername = "username";
-    private static final String databasePassword = "password";
-
-    public AbstractController(Class<T> entityClass) {
+    public AbstractController(Class<T> entityClass, EntityManager entityManager) {
         this.entityClass = entityClass;
+        this.entityManager = entityManager;
     }
 
     @Transactional
     protected synchronized void persist(T entity) throws ControllerException {
-        EntityManager entityManager = getEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
 
@@ -57,7 +45,6 @@ public abstract class AbstractController<T> {
 
     @Transactional
     protected synchronized T merge(T entity) throws ControllerException {
-        EntityManager entityManager = getEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
 
@@ -80,7 +67,6 @@ public abstract class AbstractController<T> {
 
     @Transactional
     protected synchronized void remove(T entity) throws ControllerException {
-        EntityManager entityManager = getEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
 
@@ -101,7 +87,6 @@ public abstract class AbstractController<T> {
     }
 
     protected synchronized T find(Object id) throws ControllerException {
-        EntityManager entityManager = getEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
 
@@ -126,13 +111,12 @@ public abstract class AbstractController<T> {
     }
 
     protected synchronized List<T> findAll() throws ControllerException {
-        EntityManager entityManager = getEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
 
-        CriteriaQuery<T> cq = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
+        CriteriaQuery<T> cq = entityManager.getCriteriaBuilder().createQuery(entityClass);
         cq.select(cq.from(entityClass));
-        List<T> e = getEntityManager().createQuery(cq).getResultList();
+        List<T> e = entityManager.createQuery(cq).getResultList();
 
         entityTransaction.commit();
         entityManager.close();
@@ -147,41 +131,5 @@ public abstract class AbstractController<T> {
                 asyncCallBackExceptionHandler.error(e);
             }
         }).start();
-    }
-
-    protected EntityManager getEntityManager() throws ControllerException {
-        setupEntityManagerFactory();
-        if (entityManagerFactory != null && entityManagerFactory.isOpen()) {
-            return entityManagerFactory.createEntityManager();
-        } else {
-            throw new EntityManagerNotInitializedException();
-        }
-    }
-
-    private void setupEntityManagerFactory() {
-        if (entityManagerFactory == null || !entityManagerFactory.isOpen()) {
-            Properties properties = new Properties();
-
-            if (remoteDatabase) {
-                properties.put("javax.persistence.jdbc.driver", "com.mysql.jdbc.Driver");
-                properties.put("javax.persistence.jdbc.url", "jdbc:mysql://" + databaseHost + ":" + databasePort + "/" + databaseSchema + "?autoReconnect=true");
-                properties.put("javax.persistence.jdbc.user", databaseUsername);
-                properties.put("javax.persistence.jdbc.password", databasePassword);
-                properties.put("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
-            } else {
-                properties.put("javax.persistence.jdbc.driver", "org.sqlite.JDBC");
-                properties.put("javax.persistence.jdbc.url", "jdbc:sqlite:./" + StelyTeamPlugin.getPlugin().getDataFolder().getPath() + "/database" + "?autoReconnect=true");
-                properties.put("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect");
-            }
-
-            Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
-            entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit", properties);
-        }
-    }
-
-    public static void closeEntityManagerFactory() {
-        if (entityManagerFactory != null && entityManagerFactory.isOpen()) {
-            entityManagerFactory.close();
-        }
     }
 }
