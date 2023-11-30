7a9a7d11093c33d6a11939d66143e45f320199fe
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-30 23:06:18 +0100

    ~ Remove unsed interface

--- src/main/java/fr/army/stelyteam/repository/callback/AsyncCallBackExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package fr.army.stelyteam.repository.callback;
-
-import fr.army.stelyteam.repository.exception.RepositoryException;
-
-public interface AsyncCallBackExceptionHandler {
-    void error(RepositoryException e);
-}
