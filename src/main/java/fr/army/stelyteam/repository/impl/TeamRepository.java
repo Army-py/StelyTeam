7cdb3d1f5d12cb212931cbcbc8f2a26520044861
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-30 23:01:28 +0100

    ~ Edit method to run it asynchronous

--- src/main/java/fr/army/stelyteam/repository/impl/TeamRepository.java
+++ src/main/java/fr/army/stelyteam/repository/impl/TeamRepository.java
@@ -1,9 +1,12 @@
 package fr.army.stelyteam.repository.impl;
 
+import fr.army.stelyteam.entity.impl.MemberEntity;
 import fr.army.stelyteam.repository.AbstractRepository;
 import fr.army.stelyteam.entity.impl.TeamEntity;
+import fr.army.stelyteam.repository.callback.AsyncCallBackObject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,11 +17,15 @@ public class TeamRepository extends AbstractRepository<TeamEntity> {
     }
 
 
-    public TeamEntity findByName(@NotNull String teamName) {
-        CriteriaQuery<TeamEntity> query = criteriaBuilder.createQuery(entityClass);
-        Root<TeamEntity> teamEntityRoot = query.from(entityClass);
-        query.select(teamEntityRoot);
-        query.where(criteriaBuilder.equal(teamEntityRoot.get("name"), teamName));
-        return entityManager.createQuery(query).getSingleResult();
+    public synchronized void findByTeamName(@NotNull String teamName, AsyncCallBackObject<TeamEntity> callback) {
+        executeQuery(() -> {
+                    CriteriaQuery<TeamEntity> query = criteriaBuilder.createQuery(entityClass);
+                    Root<TeamEntity> teamEntityRoot = query.from(entityClass);
+                    query.select(teamEntityRoot);
+                    query.where(criteriaBuilder.equal(teamEntityRoot.get("name"), teamName));
+                    return entityManager.createQuery(query).getSingleResult();
+                },
+                callback);
     }
+
 }
