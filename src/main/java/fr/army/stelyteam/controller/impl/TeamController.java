0d60470b41f90ef1a7d30b638ae307757966c15e
Army-py <61599555+Army-py@users.noreply.github.com>
2023-11-30 19:02:44 +0100

    ~ Add findByName method

--- src/main/java/fr/army/stelyteam/controller/impl/TeamController.java
+++ src/main/java/fr/army/stelyteam/controller/impl/TeamController.java
@@ -3,9 +3,23 @@ package fr.army.stelyteam.controller.impl;
 import fr.army.stelyteam.controller.AbstractController;
 import fr.army.stelyteam.entity.impl.TeamEntity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.jetbrains.annotations.NotNull;
 
 public class TeamController extends AbstractController<TeamEntity> {
+
     public TeamController(Class<TeamEntity> entityClass, EntityManager entityManager) {
         super(entityClass, entityManager);
+    }
+
+
+    public TeamEntity findByName(@NotNull String teamName) {
+
+        CriteriaQuery<TeamEntity> query = criteriaBuilder.createQuery(entityClass);
+        Root<TeamEntity> teamEntityRoot = query.from(entityClass);
+        query.select(teamEntityRoot);
+        query.where(criteriaBuilder.equal(teamEntityRoot.get("name"), teamName));
+        return entityManager.createQuery(query).getSingleResult();
     }
 }
