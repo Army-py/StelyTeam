87a745256bb422a044cb5000dece50b70d9cf560
Army-py <61599555+Army-py@users.noreply.github.com>
2025-01-24 22:27:00 +0100

    Removed unused methods

--- src/main/java/fr/army/stelyteam/utils/manager/serializer/ItemStackSerializer.java
+++ src/main/java/fr/army/stelyteam/utils/manager/serializer/ItemStackSerializer.java
@@ -8,35 +8,11 @@ import java.util.List;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import fr.army.stelyteam.StelyTeamPlugin;
 
 
 public class ItemStackSerializer {
-
-    public String serializeToBase64(ItemStack[] itemStack) {
-        try {
-            final ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
-            final BukkitObjectOutputStream objectOutputStream = new BukkitObjectOutputStream(arrayOutputStream);
-            objectOutputStream.writeObject(itemStack);
-            return Base64Coder.encodeLines(arrayOutputStream.toByteArray());
-        } catch (final Exception exception) {
-            throw new RuntimeException("Error turning ItemStack into base64", exception);
-        }
-    }
-
-
-    public ItemStack[] deserializeFromBase64(String base64) {
-        try {
-            final ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(Base64Coder.decodeLines(base64));
-            final BukkitObjectInputStream objectInputStream = new BukkitObjectInputStream(arrayInputStream);
-            return (ItemStack[]) objectInputStream.readObject();
-        } catch (final Exception exception) {
-            throw new RuntimeException("Error turning base64 into ItemStack", exception);
-        }
-    }
-
 
     public byte[] serializeToByte(ItemStack[] itemStack) {
         try {
